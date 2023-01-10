949955b21a758c3d638eb307b0f8382d537fe7b0
Bülent Ala <bulentala@gmail.com>
2023-01-10 12:47:50 +0300

    ok

---
+++ app/components/Pagination.js
@@ -0,0 +1,11 @@
+const pagination = () => {
+  return (
+    <div className='w-max bg-gray-50 p-6'>
+      <div className=' rounded border border-gray-200 bg-white px-2 hover:bg-gray-900 hover:text-white'>
+        1
+      </div>
+    </div>
+  );
+};
+
+export default pagination;
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Pagination from "./components/Pagination";
+const AppIndexPage = () => {
+  return (
+    <div>
+      <div>AppIndexPage</div>
+
+      <Pagination />
+    </div>
+  );
+};
+
+export default AppIndexPage;
